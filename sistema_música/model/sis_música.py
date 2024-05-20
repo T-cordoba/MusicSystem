5b686c633b66bd18aaf25e60ba58ffbf0deeb0c7
T-cordoba <tommy.cormo@gmail.com>
2024-05-20 00:34:40 -0500

    Imported json and pathlib library for read songs.json
    Imported exceptions
    
    Optimized included songs implementation.
    
    Add rename_playlist
    
    Enhanced random playlist
    
    Add recommend_playlist function

--- sistema_música/model/sis_música.py
+++ sistema_música/model/sis_música.py
@@ -1,14 +1,9 @@
 import pygame
 import random
-import os
-
-
-class AlreadyExistsError(Exception):
-    pass
-
-
-class EmptySongNameError(Exception):
-    pass
+import json
+from pathlib import Path
+from sistema_música.exceptions.exceptions import (AlreadyExistsError, EmptySongNameError, PlaylistNotFoundError,
+                                                  PlaylistAlreadyExistsError)
 
 
 class Song:
@@ -45,6 +40,18 @@ class User:
         self.country = country
         self.playlists: dict[str, Playlist] = {}
 
+    def rename_playlist(self, old_name, new_name):
+        if old_name not in self.playlists:
+            raise PlaylistNotFoundError(f'No existe una playlist con el nombre {old_name}')
+        elif new_name in self.playlists:
+            raise PlaylistAlreadyExistsError(f'Ya existe una playlist con el nombre {new_name}')
+
+        for playlist in self.playlists:
+            if playlist == old_name:
+                self.playlists[new_name] = self.playlists.pop(playlist)
+                self.playlists[new_name].name = new_name
+                break
+
     def create_playlist(self, name: str):
         self.playlists[name] = Playlist([], name)
 
@@ -69,6 +76,9 @@ class AudioPlayer:
         self.is_playing = False
         pygame.mixer.init()
         pygame.mixer.music.set_volume(self.volume)
+
+    def is_music_playing(self):
+        return pygame.mixer.music.get_busy()
 
     def play(self, song: Song):
         pygame.mixer.music.load(song.file_path)
@@ -111,146 +121,64 @@ class AudioPlayer:
 class SysMusic:
     def __init__(self, audio_player: AudioPlayer):
         self.audio_player = audio_player
-        self.songs: list[Song] = [
-            Song('bad guy', 'Billie Eilish', 'Pop',
-                 os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(__file__))), 'assets',
-                              'Billie Eilish - bad guy.mp3')),
-            Song('Kiss Me More', 'Doja Cat ft. SZA', 'Pop',
-                 os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(__file__))), 'assets',
-                              'Doja Cat - Kiss Me More.mp3')),
-            Song('Levitating', 'Dua Lipa', 'Pop',
-                 os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(__file__))), 'assets',
-                              'Dua Lipa - Levitating.mp3')),
-            Song('Shape of You', 'Ed Sheeran', 'Pop',
-                 os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(__file__))), 'assets',
-                              'Ed Sheeran - Shape of You.mp3')),
-            Song('As it was', 'Harry Styles', 'Pop',
-                 os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(__file__))), 'assets',
-                              'Harry Styles - As It Was.mp3')),
-            Song('Roar', 'Katy Perry', 'Pop',
-                 os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(__file__))), 'assets',
-                              'Katy Perry - Roar.mp3')),
-            Song('Bad Romance', 'Lady Gaga', 'Pop',
-                 os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(__file__))), 'assets',
-                              'Lady Gaga - Bad Romance.mp3')),
-            Song('Sugar', 'Maroon 5', 'Pop',
-                 os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(__file__))), 'assets',
-                              'Maroon 5 - Sugar.mp3')),
-            Song('Billie Jean', 'Michael Jackson', 'Pop',
-                 os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(__file__))), 'assets',
-                              'Michael Jackson - Billie Jean.mp3')),
-            Song('good 4 u', 'Olivia Rodrigo', 'Pop',
-                 os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(__file__))), 'assets',
-                              'Olivia Rodrigo - good 4 u.mp3')),
-            Song('Sunflower', 'Post Malone ft. Swae Lee', 'Pop',
-                 os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(__file__))), 'assets',
-                              'Post Malone & Swae Lee - Sunflower.mp3')),
-            Song('Bohemian Rhapsody', 'Queen', 'Pop',
-                 os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(__file__))), 'assets',
-                              'Queen - Bohemian Rhapsody.mp3')),
-            Song('Leave the Door Open', 'Silk Sonic', 'Pop',
-                 os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(__file__))), 'assets',
-                              'Silk Sonic - Leave the Door Open.mp3')),
-            Song('Blank Space', 'Taylor Swift', 'Pop',
-                 os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(__file__))), 'assets',
-                              'Taylor Swift - Blank Space.mp3')),
-            Song('Blinding Lights', 'The Weeknd', 'Pop',
-                 os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(__file__))), 'assets',
-                              'The Weeknd - Blinding Lights.mp3')),
-            Song('Back in Black', 'AC/DC', 'Rock',
-                 os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(__file__))), 'assets',
-                              'ACDC - Back in Black.mp3')),
-            Song('Sweet Child O Mine', 'Guns N Roses', 'Rock',
-                 os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(__file__))), 'assets',
-                              'Guns N Roses - Sweet Child o Mine.mp3')),
-            Song('Smells Like Teen Spirit', 'Nirvana', 'Rock',
-                 os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(__file__))), 'assets',
-                              'Nirvana - Smells Like Teen Spirit.mp3')),
-            Song('Radioactive', 'Imagine Dragons', 'Rock',
-                 os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(__file__))), 'assets',
-                              'Imagine Dragons - Radioactive.mp3')),
-            Song('Everlong', 'Foo Fighters', 'Rock',
-                 os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(__file__))), 'assets',
-                              'Foo Fighters - Everlong.mp3')),
-            Song('In the End', 'Linkin Park', 'Rock',
-                 os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(__file__))), 'assets',
-                              'Linkin Park - In the End.mp3')),
-            Song('Stairway to Heaven', 'Led Zeppelin', 'Rock',
-                 os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(__file__))), 'assets',
-                              'Led Zeppelin - Stairway to Heaven.mp3')),
-            Song('Enter Sandman', 'Metallica', 'Rock',
-                 os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(__file__))), 'assets',
-                              'Metallica - Enter Sandman.mp3')),
-            Song('Hey Jude', 'The Beatles', 'Rock',
-                 os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(__file__))), 'assets',
-                              'The Beatles - Hey Jude.mp3')),
-            Song('Wish You Were Here', 'Pink Floyd', 'Rock',
-                 os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(__file__))), 'assets',
-                              'Pink Floyd - Wish You Were Here.mp3')),
-            Song('One more time', 'Daft Punk', 'Electronica',
-                 os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(__file__))), 'assets',
-                              'Daft Punk - One More Time.mp3')),
-            Song('Feel so close', 'Calvin Harris', 'Electronica',
-                 os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(__file__))), 'assets',
-                              'Calvin Harris - Feel So Close.mp3')),
-            Song('Wake me up', 'Avicii', 'Electronica',
-                 os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(__file__))), 'assets',
-                              'Avicii - Wake Me Up.mp3')),
-            Song('Dont you worry child', 'Swedish House Mafia', 'Electronica',
-                 os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(__file__))), 'assets',
-                              'Swedish House Mafia - Dont You Worry Child.mp3')),
-            Song('Animals', 'Martin Garrix', 'Electronica',
-                 os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(__file__))), 'assets',
-                              'Martin Garrix - Animals.mp3')),
-            Song('Ghost n Stuff', 'Deadmau5', 'Electronica',
-                 os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(__file__))), 'assets',
-                              'Deadmau5 - Ghosts n Stuff.mp3')),
-            Song('D.A.N.C.E.', 'Justice', 'Electronica',
-                 os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(__file__))), 'assets',
-                              'Justice - D.A.N.C.E..mp3')),
-            Song('Galvanize', 'The Chemical Brothers', 'Electronica',
-                 os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(__file__))), 'assets',
-                              'The Chemical Brothers - Galvanize.mp3')),
-            Song('HUMBLE.', 'Kendrick Lamar', 'Hip Hop',
-                 os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(__file__))), 'assets',
-                              'Kendrick Lamar - HUMBLE..mp3')),
-            Song('Gods Plan', 'Drake', 'Hip Hop',
-                 os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(__file__))), 'assets',
-                              'Drake - Gods Plan.mp3')),
-            Song('Stronger', 'Kanye West', 'Hip Hop',
-                 os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(__file__))), 'assets',
-                              'Kanye West - Stronger.mp3')),
-            Song('Lose Yourself', 'Eminem', 'Hip Hop',
-                 os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(__file__))), 'assets',
-                              'Eminem - Lose Yourself.mp3')),
-            Song('Empire State of Mind', 'Jay Z ft. Alicia', 'Hip Hop',
-                 os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(__file__))), 'assets',
-                              'Jay-Z - Empire State of Mind.mp3')),
-            Song('Changes', '2Pac', 'Hip Hop',
-                 os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(__file__))), 'assets',
-                              'Tupac Shakur - Changes.mp3')),
-        ]
+        self.songs = self.load_songs()
         self.user = None
+
+    def load_songs(self):
+        songs_data_path = (Path(__file__).resolve().parents[2] / 'assets' / 'songs.json')
+        with open(songs_data_path, 'r') as f:
+            songs_data = json.load(f)
+
+        songs = []
+        for song_data in songs_data:
+            song = Song(
+                song_data['title'],
+                song_data['artist'],
+                song_data['genre'],
+                str((Path(__file__).resolve().parents[2] / 'assets' / str(song_data['file_path'])))
+            )
+            songs.append(song)
+
+        return songs
 
     def add_song(self, song: Song):
         for existing_song in self.songs:
-            if existing_song.title == song.title and existing_song.artist == song.artist and existing_song.genre == song.genre:
+            if (existing_song.title == song.title and
+                    existing_song.artist == song.artist and existing_song.genre == song.genre):
                 raise AlreadyExistsError("La canción ya existe en la lista de canciones.")
         self.songs.append(song)
 
     def create_random_playlist(self):
         if len(self.songs) < 10:
             raise ValueError("No hay suficientes canciones para crear una lista de reproducción aleatoria.")
-        i=0
+        playlist_name = "Random Playlist"
+        i = 1
+        while playlist_name in self.user.playlists:
+            playlist_name = f"Random Playlist {i}"
+            i += 1
         random_songs = random.sample(self.songs, 10)
-        random_playlist = Playlist(random_songs, "Random Playlist")
+        random_playlist = Playlist(random_songs, playlist_name)
+        self.user.playlists[playlist_name] = random_playlist
 
+    def recommend_playlist(self, reference_playlist_name: str):
+        if reference_playlist_name not in self.user.playlists:
+            raise ValueError("Playlist de referencia no encontrada.")
+        reference_playlist = self.user.playlists[reference_playlist_name]
+        num_songs = min(5, len(reference_playlist.songs))
+        sampled_songs = random.sample(reference_playlist.songs, num_songs)
+        genres = {song.genre for song in sampled_songs}
+        potential_songs = [song for song in self.songs if song.genre in genres and song not in reference_playlist.songs]
+        if len(potential_songs) < 10:
+            raise ValueError("No hay suficientes canciones para crear una lista de reproducción recomendada.")
+        recommended_songs = random.sample(potential_songs, 10)
+        playlist_name = f"Playlist recomendada ({', '.join(genres)})"
+        recommended_playlist = Playlist(recommended_songs, playlist_name)
+        self.user.playlists[playlist_name] = recommended_playlist
+
+        return recommended_playlist
 
     def remove_song(self, song: Song):
         self.songs.remove(song)
 
     def set_user(self, user: User):
         self.user = user
-
-
-
